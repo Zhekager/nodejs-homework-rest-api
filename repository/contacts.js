6910658033a61db9a70a4f4c051c6dae646181e0
Yevhenii Herasymenko <74263798+Zhekager@users.noreply.github.com>
2021-10-24 22:52:06 +0300

    adds 4 homework

--- repository/contacts.js
+++ repository/contacts.js
@@ -1,17 +1,57 @@
-const Contact = require('../model/contact');
+const Contact = require("../model/contact");
 
-const listContacts = async () => {
-  const result = await Contact.find({});
+const listContacts = async (userId, query) => {
+  const {
+    sortBy,
+    sortByDesc,
+    filter,
+    favorite = null,
+    limit = 15,
+    page = 1,
+    offset = 0,
+  } = query;
+
+  const searchOptions = { owner: userId };
+
+  if (favorite !== null) {
+    searchOptions.favorite = favorite;
+  }
+
+  const results = await Contact.paginate(searchOptions, {
+    limit,
+    page,
+    offset,
+    sort: {
+      ...(sortBy ? { [`${sortBy}`]: 1 } : {}),
+      ...(sortByDesc ? { [`${sortByDesc}`]: -1 } : {}),
+    },
+    select: filter ? filter.split("|").join(" ") : "",
+    populate: {
+      path: "owner",
+      select: "name email subscription",
+    },
+  });
+  const { docs: contacts } = results;
+  delete results.docs;
+  return { ...results, contacts };
+};
+
+const getContactById = async (contactId, userId) => {
+  const result = await Contact.findOne({
+    _id: contactId,
+    owner: userId,
+  }).populate({
+    path: "owner",
+    select: "name email subscription",
+  });
   return result;
 };
 
-const getContactById = async (contactId) => {
-  const result = await Contact.findById(contactId);
-  return result;
-};
-
-const removeContact = async (contactId) => {
-  const result = await Contact.findByIdAndRemove(contactId);
+const removeContact = async (contactId, userId) => {
+  const result = await Contact.findOneAndRemove({
+    _id: contactId,
+    owner: userId,
+  });
   return result;
 };
 
@@ -20,8 +60,12 @@ const addContact = async (body) => {
   return result;
 };
 
-const updateContact = async (contactId, body) => {
-  const result = await Contact.findByIdAndUpdate(contactId, { ...body }, {new: true},);
+const updateContact = async (contactId, body, userId) => {
+  const result = await Contact.findOneAndUpdate(
+    { _id: contactId, owner: userId },
+    { ...body },
+    { new: true }
+  );
   return result;
 };
 
